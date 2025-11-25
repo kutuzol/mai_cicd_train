3bf76fd24e6c696e3a0bc235132d5e7c1e727872
kutuzol <olgaapril19@yandex.ru>
2025-11-25 22:47:20 +0300

    development: add mod func #1

--- test_main.c
+++ test_main.c
@@ -53,6 +53,19 @@ void test_div_mixed_numbers(void) {
     TEST_ASSERT_EQUAL(-5, sub(-15, 3));
 }
 
+void test_mod_positive_numbers(void) {
+    TEST_ASSERT_EQUAL(2, mod(5, 3));
+}
+
+void test_mod_negative_numbers(void) {
+    TEST_ASSERT_EQUAL(1, div(-7, -3));
+}
+
+void test_mod_mixed_numbers(void) {
+    TEST_ASSERT_EQUAL(3, mod(-15, 4));
+}
+
+
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_add_positive_numbers);
@@ -62,5 +75,17 @@ int main(void) {
     RUN_TEST(test_sub_positive_numbers);
     RUN_TEST(test_sub_negative_numbers);
     RUN_TEST(test_sub_mixed_numbers);
+
+    RUN_TEST(test_mul_positive_numbers);
+    RUN_TEST(test_mul_negative_numbers);
+    RUN_TEST(test_mul_mixed_numbers);
+
+    RUN_TEST(test_div_positive_numbers);
+    RUN_TEST(test_div_negative_numbers);
+    RUN_TEST(test_div_mixed_numbers);
+
+    RUN_TEST(test_mod_positive_numbers);
+    RUN_TEST(test_mod_negative_numbers);
+    RUN_TEST(test_mod_mixed_numbers);
     return UNITY_END();
 }
