c3bdefa6a6eae694af0544055b96379a848c346a
kutuzol <olgaapril19@yandex.ru>
2025-11-26 23:04:03 +0300

    test-update doxygen documentation in release v1.0

--- main.h
+++ main.h
@@ -1,3 +1,9 @@
+/**
+ * @file main.h
+ * @brief Заголовочный файл
+ * @details 
+ */
+
 #include <stdio.h>
 
 /**
@@ -41,3 +47,70 @@ int div(int a, int b);
  * @note Делитель не должен быть 0.
  */
 int mod(int a, int b);
+
+
+/**
+ * @brief Побитовое И (AND)
+ *  Это только прототипы функций, реализация будет добавлена позже.
+ * Выполняет побитовое логическое умножение двух целых чисел.
+ * 
+ * @param a Первое число
+ * @param b Второе число
+ * @return Результат операции a & b
+ */
+unsigned int bitwise_and(unsigned int a, unsigned int b);
+
+/**
+ * @brief Побитовое ИЛИ (OR)
+ * 
+ * Выполняет побитовое логическое сложение двух целых чисел.
+ * 
+ * @param a Первое число
+ * @param b Второе число
+ * @return Результат операции a | b
+ */
+
+unsigned int bitwise_or(unsigned int a, unsigned int b);
+
+/**
+ * @brief Побитовое исключающее ИЛИ (XOR)
+ * 
+ * Выполняет побитовое сложение по модулю 2 двух целых чисел.
+ * 
+ * @param a Первое число
+ * @param b Второе число
+ * @return Результат операции a ^ b
+ */
+unsigned int bitwise_xor(unsigned int a, unsigned int b);
+
+/**
+ * @brief Побитовое отрицание (NOT)
+ * 
+ * Вычисляет побитовое отрицание числа.
+ * 
+ * @param a Число для инверсии
+ * @return Результат операции ~a
+ */
+unsigned int bitwise_not(unsigned int a);
+
+/**
+ * @brief Побитовый сдвиг влево
+ * 
+ * Сдвигает число влево на n бит.
+ * 
+ * @param a Число для сдвига
+ * @param n Количество бит сдвига
+ * @return Результат сдвига a << n
+ */
+unsigned int bitwise_left_shift(unsigned int a, unsigned int n);
+
+/**
+ * @brief Побитовый сдвиг вправо
+ * 
+ * Сдвигает число вправо на n бит.
+ * 
+ * @param a Число для сдвига
+ * @param n Количество бит сдвига
+ * @return Результат сдвига a >> n
+ */
+unsigned int bitwise_right_shift(unsigned int a, unsigned int n);
